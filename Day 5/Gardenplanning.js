16dcb1a898a9d6659e674fe387a7f5e8f499f7b0
Gogulakumar <113664713+Gogulakumar@users.noreply.github.com>
2023-07-13 15:21:22 +0530

    Add files via upload

---
+++ Day 5/Gardenplanning.js
@@ -0,0 +1,35 @@
+import React from 'react'
+import './Gardenplanning.css';
+// import Navbar from './Navbar';
+function Home() {
+  return (
+    <div>
+      {/* <Navbar/> */}
+  <header class="header">
+    <a href="#" class="logo">HomeGarden</a>
+    <nav class="nav-items">
+      <a href="Home">Home</a>
+      <a href="#">Season</a>
+      <a href="#">Remainder</a>
+      <a href="#">Garden Journal</a>
+      <a href="#">About</a>
+      <a href="#">Contact</a> 
+      <a href="login">Login</a>
+      <a href="reg">Register</a>
+    </nav>
+  </header>
+  <main>
+    <div class="intro2">
+     
+      <p></p>
+     
+    </div>
+    <div class="about-me">
+    </div>
+    </main>
+  
+  </div>
+)
+}
+
+export default Home
